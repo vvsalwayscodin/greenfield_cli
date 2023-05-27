4b8dcffa8e24f7b42400ccd2925bfdd3b2e06211
dumanmonster <dybysbayd@gmail.com>
2023-05-27 20:13:36 +0600

    bucket: INIT

--- cmd/main.py
+++ cmd/main.py
@@ -1,8 +1,11 @@
 import click
+
+from cmd_bucket import bucket
 from cmd_config import config
-from cmd_keystore import create_keystore
 from cmd_crosschain import crosschain
+from cmd_keystore import create_keystore
 from cmd_sp import sp
+
 
 @click.group()
 def gnfd():
@@ -13,3 +16,4 @@ gnfd.add_command(config)
 gnfd.add_command(create_keystore, 'create-keystore')
 gnfd.add_command(crosschain)
 gnfd.add_command(sp)
+gnfd.add_command(bucket)
