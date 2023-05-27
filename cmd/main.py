dd8ed6d9e5c30857583c0b2d2f1d021062c27fba
dumanmonster <dybysbayd@gmail.com>
2023-05-27 19:27:09 +0600

    SP: fix

--- cmd/main.py
+++ cmd/main.py
@@ -2,7 +2,7 @@ import click
 from cmd_config import config
 from cmd_keystore import create_keystore
 from cmd_crosschain import crosschain
-
+from cmd_sp import sp
 
 @click.group()
 def gnfd():
@@ -12,3 +12,4 @@ def gnfd():
 gnfd.add_command(config)
 gnfd.add_command(create_keystore, 'create-keystore')
 gnfd.add_command(crosschain)
+gnfd.add_command(sp)
