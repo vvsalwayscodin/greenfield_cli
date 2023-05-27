1be708a24c5bcb5409d04348f144d1c9848a72db
dumanmonster <dybysbayd@gmail.com>
2023-05-27 20:28:57 +0600

    payment: INIT

---
+++ cmd/cmd_payment.py
@@ -0,0 +1,41 @@
+import click
+
+
+@click.group('payment')
+def payment():
+    pass
+
+@click.command('create_account')
+def create_account():
+    click.echo('create account logic')
+
+
+@click.command('deposit')
+@click.option('--to_address', type="str", help="the stream account", prompt='Enter the stream account')
+@click.option('--amount', type="str", help="the amount to be deposited", default="",
+              prompt='Enter the amount to be deposited')
+def deposit(to_address, amount):
+    click.echo(f"{amount}")
+
+
+@click.command('withdraw')
+@click.option('--from_address', type="str", help="the stream account", prompt='Enter the stream account')
+@click.option('--amount', type="str", help="the amount to be deposited", default="",
+              prompt='Enter the amount to be deposited')
+def withdraw(to_address, amount):
+    click.echo(f"{amount}")
+
+
+@click.command('ls')
+@click.option('--owner_address', type="str", default="",
+              help="indicate a owner's payment accounts to be list, account address can be omitted for current user's accounts listing",
+              required=False)
+def ls(owner_address):
+    click.echo(f"{owner_address}")
+
+
+payment.add_command(create_account)
+payment.add_command(deposit)
+payment.add_command(withdraw)
+payment.add_command(ls)
+
--- cmd/main.py
+++ cmd/main.py
@@ -4,6 +4,7 @@ from cmd_bucket import bucket
 from cmd_config import config
 from cmd_crosschain import crosschain
 from cmd_keystore import create_keystore
+from cmd_payment import payment
 from cmd_sp import sp
 
 
@@ -17,3 +18,4 @@ gnfd.add_command(create_keystore, 'create-keystore')
 gnfd.add_command(crosschain)
 gnfd.add_command(sp)
 gnfd.add_command(bucket)
+gnfd.add_command(payment)
