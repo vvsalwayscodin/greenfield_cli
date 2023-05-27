211d38a9d5be573be134ef3c9e9e3a1e6846c55f
dumanmonster <dybysbayd@gmail.com>
2023-05-27 18:55:18 +0600

    create_keystore FIXED (Ready to do function)

--- cmd/cmd_keystore.py
+++ cmd/cmd_keystore.py
@@ -2,9 +2,7 @@ import click
 
 
 @click.command()
-@click.option('-pkf', '--privateKeyFile', type=str, help="Set private key file path",
-              prompt="Enter path to private key file")
-@click.option('-gkj', '--generatedKeyJson', type=str, help="Set name of generated key.json",
-              prompt="Enter name of generating key json file")
-def create_keystore(privateKeyFile, generatedKeyJson):
-    click.echo(f"{privateKeyFile, generatedKeyJson}")
+@click.option('-pkf', '--private_key_file', default='../key.txt', help='Private key file path')
+@click.argument('new_file_name')
+def create_keystore(private_key_file, new_file_name):
+    click.echo(f"{private_key_file}")
--- cmd/main.py
+++ cmd/main.py
@@ -2,11 +2,11 @@ import click
 from cmd_config import config
 from cmd_keystore import create_keystore
 
+
 @click.group()
 def gnfd():
     pass
 
 
 gnfd.add_command(config)
-gnfd.add_command(create_keystore)
-
+gnfd.add_command(create_keystore, 'create-keystore')
